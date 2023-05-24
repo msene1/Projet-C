c9e4ce37cc787abbadedc93bd255b5eeccb85089
msene1 <martial.sene@free.fr>
2023-05-24 15:09:01 +0200

    maj exo1

--- BETA_C.c
+++ BETA_C.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+
 int main()
 {
     char ville[20], sexe[8], prenom[20], nom[20];
-   int age, code_postale, taille;
+    int age, code_postale, taille;
 
     printf("Entrer votre Nom\n");
-    scanf("%s", &nom);
+    scanf("%s", nom);
     printf("Entrer votre Prénom\n");
-    scanf("%s", &prenom);
-    printf("Entrer votre Sexe\n");
-    scanf("%s", &sexe);
+    scanf("%s", prenom);
+    printf("Entrer votre Sexe (homme/femme)\n");
+    scanf("%s", sexe);
     printf("Entrer votre Age\n");
     scanf("%d", &age);
 
-    printf("Le personne s'appelle %s, %s c'est une personne de sexe %s a %d ans.  \n", nom, prenom, sexe, age);
+    // Vérifier si le genre est correctement respecté
+    if ((strcmp(sexe, "homme") == 0 && age >= 18) || (strcmp(sexe, "femme") == 0 && age >= 16))
+    {
+        printf("La personne s'appelle %s, %s, c'est une personne de sexe %s et elle est majeure.\n", nom, prenom, sexe);
+    }
+    else if ((strcmp(sexe, "homme") == 0 && age < 18) || (strcmp(sexe, "femme") == 0 && age < 16))
+    {
+        printf("La personne s'appelle %s, %s, c'est une personne de sexe %s et elle est mineure.\n", nom, prenom, sexe);
+    }
+    else
+    {
+        printf("Le genre spécifié n'est pas valide.\n");
+    }
+
     return 0;
 }
